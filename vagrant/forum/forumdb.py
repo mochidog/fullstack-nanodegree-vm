a99bb69169cc473d8bd242b09f8de1938427a85a
Lei Zhang <lei.physics@gmail.com>
2015-10-25 18:15:23 -0700

    fixed forumdb.py security loopholes

--- vagrant/forum/forumdb.py
+++ vagrant/forum/forumdb.py
@@ -3,9 +3,12 @@
 # 
 
 import time
+import psycopg2
+import bleach
 
 ## Database connection
-DB = []
+#DB = []
+DB = "dbname=forum"
 
 ## Get posts from database.
 def GetAllPosts():
@@ -16,8 +19,15 @@ def GetAllPosts():
       pointing to the post content, and 'time' key pointing to the time
       it was posted.
     '''
-    posts = [{'content': str(row[1]), 'time': str(row[0])} for row in DB]
-    posts.sort(key=lambda row: row['time'], reverse=True)
+    #posts = [{'content': str(row[1]), 'time': str(row[0])} for row in DB]
+    #posts.sort(key=lambda row: row['time'], reverse=True)
+    conn = psycopg2.connect(DB)
+    query = "select * from posts order by time desc"
+    c = conn.cursor()
+    c.execute(query)
+    rows = c.fetchall()
+    posts = [{'content': bleach.clean(row[0]).encode('ascii', 'ignore'), 'time': row[1].strftime('%y-%m-%d')} for row in rows]
+    conn.close()
     return posts
 
 ## Add a post to the database.
@@ -27,5 +37,10 @@ def AddPost(content):
     Args:
       content: The text content of the new post.
     '''
-    t = time.strftime('%c', time.localtime())
-    DB.append((t, content))
+    conn = psycopg2.connect(DB)
+    #t = time.strftime('%c', time.localtime())
+    #DB.append((t, content))
+    c = conn.cursor()
+    c.execute("insert into posts (content) values (%s)", (content, ))
+    conn.commit()
+    conn.close()
